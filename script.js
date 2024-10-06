7a7d7b5e63115c5e852d74ab94bc62ff2dc457f4
Emanuel Rylke <ema@mailbox.org>
2024-10-06 18:44:24 +0200

    Fix scaling on mobile

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ addEventListener('DOMContentLoaded', () => {
     for (let i = 0; i < 20; i++) {
         let img = document.createElement('img');
         img.src = ((Math.random() * 3) | 0) + '.png';
-        document.body.append(img);
+        document.querySelector('div').append(img);
     }
 
 });
